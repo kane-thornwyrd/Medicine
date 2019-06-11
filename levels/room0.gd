6e2a1459479abdbc2fb3fdd4af6f9c59f07ab40f
Jean-cédric THÉROND <jean.cedric.t@gmail.com>
2019-06-11 09:46:42 +0200

    old pathfinding

--- levels/room0.gd
+++ levels/room0.gd
@@ -1,13 +1,37 @@
 extends Node2D
 
-onready var nav2D:Navigation2D = $nav2D
-onready var player:KinematicBody2D = $player
-onready var line:Line2D = $line2D
+#onready var nav2D:Navigation2D = $nav2D
+#onready var player:KinematicBody2D = $nav2D/grid/player
+#onready var grid:TileMap = $nav2D/grid
+#onready var line:Line2D = $line2D
 
-func _unhandled_input(event:InputEvent) -> void:
-  if not event is InputEventMouseButton: return;
-  if event.button_index != BUTTON_LEFT or not event.pressed: return;
-
-  var new_path:PoolVector2Array = nav2D.get_simple_path(player.global_position, event.global_position)
-  line.points = new_path
-  player.path = new_path
+#func _unhandled_input(event:InputEvent) -> void:
+#  if not event is InputEventMouseButton: return;
+#  if event.button_index != BUTTON_LEFT or not event.pressed: return;
+#
+#  var evpos = grid.update_child_pos(event.position)
+#
+#  var new_path:PoolVector2Array = nav2D.get_simple_path(player.position, evpos, false)
+#
+#  for i in range(new_path.size()):
+#    new_path[i] = grid.update_child_pos(new_path[i])
+#    var next_dir:Vector2
+#    if i+1 != new_path.size():
+#      next_dir = grid.world_to_map(new_path[i])\
+#        .direction_to(grid.world_to_map(new_path[i+1]))\
+#        .round()
+#    else:
+#      next_dir = grid.world_to_map(new_path[i])\
+#        .direction_to(grid.world_to_map(evpos) + Vector2(0,32))\
+#        .round()
+#
+#    while int(next_dir.x) != 0 and int(next_dir.y) != 0:
+#      new_path[i] = grid.update_child_pos(new_path[i], Vector2(next_dir.x, 0))
+#      new_path.insert(i+1, grid.update_child_pos(new_path[i], Vector2(0, next_dir.y)))
+#      i += 1
+#      next_dir = new_path[i].direction_to(new_path[i+1]).round()
+#
+#    print_debug(new_path)
+#
+#  line.points = new_path
+#  player.path = new_path
--- script/player.gd
+++ script/player.gd
@@ -1,31 +1,75 @@
 extends KinematicBody2D
 
-var speed:float = 400
-var path:PoolVector2Array = PoolVector2Array() setget set_path
+export var grid_path:NodePath
+
+onready var grid = self.get_node(grid_path)
+
+var direction = Vector2()
+
+const MAX_SPEED = 400
+
+var speed = 0
+var velocity:Vector2 = Vector2()
+#var path:PoolVector2Array = PoolVector2Array() setget set_path
+var world_target_pos = Vector2()
+var target_direction = Vector2()
+var pos_on_grid:Vector2
+var is_moving:bool = false
+var is_moving_by_touch:bool = false
+var type
 
 func _ready() -> void:
-  set_process(false)
+  set_physics_process(true)
+  self.type = grid.PLAYER
 
-func _process(delta: float) -> void:
-  var move_distance:float = speed * delta
-  move_along_path(move_distance)
+func _physics_process(delta:float) -> void:
+  direction = Vector2()
+  speed = 0
 
-func move_along_path(distance:float) -> void:
-  var start_point = self.position
-  for i in range(path.size()):
-    var distance_to_next:float = start_point.distance_to(path[0])
-    if distance <= distance_to_next and distance >= 0.0:
-      position = start_point.linear_interpolate(path[0], distance / distance_to_next)
-      break
-    elif distance < 0.0:
-      self.position = path[0]
-      set_process(false)
-      break
-    distance -= distance_to_next
-    start_point = path[0]
-    path.remove(0)
+#  for i in range(path.size()) :
+#    target_direction = self.position.direction_to(path[0]).round()
+#    if grid.is_cell_vacant(path[0], target_direction):
+#      world_target_pos = grid.update_child_pos(path[0], target_direction, self.type)
+#      self.position = self.move_and_slide(world_target_pos, Vector2.ZERO,true, 0,0.785398,false)
+#      path.remove(0)
 
-func set_path(new_path:PoolVector2Array) -> void:
-  path = new_path
-  if new_path.size() == 0: return;
-  set_process(true)
+  if Input.is_action_pressed("ui_up"):
+    direction = Vector2.UP
+  elif Input.is_action_pressed("ui_down"):
+    direction = Vector2.DOWN
+  elif Input.is_action_pressed("ui_left"):
+    direction = Vector2.LEFT
+  elif Input.is_action_pressed("ui_right"):
+    direction = Vector2.RIGHT
+
+  if not is_moving and direction != Vector2():
+    target_direction = direction.normalized()
+    if grid.is_cell_vacant(position, direction):
+      world_target_pos = grid.update_child_pos(self.position, direction, self.type)
+      is_moving = true
+
+  elif is_moving:
+    speed = MAX_SPEED
+    velocity = speed * target_direction * delta
+    var vel = self.move_and_slide(world_target_pos, Vector2.ZERO,true, 0,0.785398,false)
+    self.position = vel
+    is_moving = false
+
+#func move_along_path(distance:float) -> void:
+#  var start_point = self.position
+#  for i in range(path.size()):
+#    var distance_to_next:float = start_point.distance_to(path[0])
+#    if distance <= distance_to_next and distance >= 0.0:
+#      var vel = self.move_and_slide(path[0], Vector2.ZERO,true, 0,0.785398,false)
+#      position = start_point.linear_interpolate(vel, distance / distance_to_next).round()
+#      break
+#    elif distance < 0.0:
+#      self.position = path[0]
+#      break
+#    distance -= distance_to_next
+#    start_point = path[0]
+#    path.remove(0)
+
+#func set_path(new_path:PoolVector2Array) -> void:
+#  path = new_path
+#  if new_path.size() == 0: return;
