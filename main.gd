ab0721bf27fd1e988145b03b3e4cefb88b711d18
Jean-cédric THÉROND <jean.cedric.t@gmail.com>
2019-06-15 10:07:54 +0200

    Removed the Thread loading, because Threads don't mix with web and win10

--- main.gd
+++ main.gd
@@ -2,19 +2,17 @@ extends Control
 
 export var loading_scene:PackedScene
 
-onready var loading_scene_thread = Thread.new()
-onready var loading_room_thread = Thread.new()
+#onready var loading_thread = Thread.new()
 
 var all_loaded:Array = [false,false]
 
 func _ready() -> void:
   REG.add_scene("loading_screen", loading_scene)
   var loading_i = REG.get_scene("loading_screen").instance()
-  loading_scene_thread.start(self, "load_scene_data", loading_i)
-  loading_room_thread.start(self, "load_room_data", loading_i)
   self.add_child(loading_i)
+  load_data(loading_i)
 
-func load_scene_data(loading_i:Node) -> int:
+func load_data(loading_screen:Node) -> int:
   var scenes_dir = Directory.new()
   scenes_dir.open("res://scenes")
   scenes_dir.list_dir_begin(true, true)
@@ -26,10 +24,7 @@ func load_scene_data(loading_i:Node) -> int:
     scene_file = scenes_dir.get_next()
   print_debug(REG._scenes.keys())
   all_loaded[0] = true
-  _refresh_loading_state(loading_i)
-  return 0
 
-func load_room_data(loading_i:Node) -> int:
   var rooms_dir = Directory.new()
   rooms_dir.open("res://levels")
   rooms_dir.list_dir_begin(true, true)
@@ -42,8 +37,9 @@ func load_room_data(loading_i:Node) -> int:
     room_file = rooms_dir.get_next()
   print_debug(ROOM_REG._rooms.keys())
   all_loaded[1] = true
-  _refresh_loading_state(loading_i)
+
+  _refresh_loading_state(loading_screen)
   return 0
 
 func _refresh_loading_state(loading_i:Node) -> void:
-  loading_i.is_loading = not all_loaded.has(false)
+  loading_i.is_loading = all_loaded.has(false)
